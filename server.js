12c11938767b9ad64e8a0a5f8c97cee7d45e95df
Ashmit Sharma <alpha5sector@gmail.com>
2025-12-30 03:47:54 +0530

    might fix the cors issue

--- server.js
+++ server.js
@@ -32,7 +32,12 @@ app.use(
 );
 
 // Handle preflight requests
-app.options("*", (req, res) => res.sendStatus(200));
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.sendStatus(200);
 
 // ===========================
 //      Supabase Setup
